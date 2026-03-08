8f7b8d1596c76e1f3367a80329e5b3a6ff901ffe
hhbby <109127090+iiiiiiiword@users.noreply.github.com>
2026-03-08 22:20:58 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -32,11 +32,12 @@ def main(args):
         args.style_images,
         steps=args.noise_steps,
         content_original_mask=content_original_mask,
-        use_depth=False,
-        use_sam=False  
+        use_sam=args.use_sam,
+        use_depth=args.use_depth 
     )
 
     if args.print:
+        print("----------------------------------------------------------------------------------------------")
         match_summary = {}
         for content_cluster, match_info in cluster_matches.items():
             match_summary[content_cluster] = {
@@ -63,18 +64,20 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="StyleGallery Inference Script")
     parser.add_argument("--model_name", type=str, default="pretrained_models/runwayml_stable-diffusion-v1-5", help="path to SD 1.5")
-    parser.add_argument("--content_image", type=str, default="content/natural_scene/0001.png", help="content image path")
-    parser.add_argument("--style_images", type=str, nargs='+', default=["style/NormalStyle/NS003.png", "style/NormalStyle/NS004.png"], help="List of style image paths")
+    parser.add_argument("--content_image", type=str, default="content/city_scene/0004.png", help="content image path")
+    parser.add_argument("--style_images", type=str, nargs='+', default=["style/PixelArt/PA008.png"], help="List of style image paths")
     parser.add_argument("--content_mask_path", type=str, default=None, help="additional mask path")
     parser.add_argument("--output_folder", type=str, default="outputs", help="output folder path")
 
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--use_sam", action="store_true", default=False, help="Simply to verify scalability, perhaps not be very practical")
+    parser.add_argument("--use_depth", action="store_true", default=True)
     parser.add_argument("--start_layer", type=int, default=10)
     parser.add_argument("--end_layer", type=int, default=16)
     parser.add_argument("--noise_steps", type=int, default=15)
     parser.add_argument("--num_optimize_steps", type=int, default=150)
     parser.add_argument("--c_ratio", type=float, default=0.26)
-    parser.add_argument("--print", type=bool, default=False, help="show cluster mathcing results")
+    parser.add_argument("--print", action="store_true", default=True, help="show cluster mathcing results")
 
     args = parser.parse_args()
     main(args)
